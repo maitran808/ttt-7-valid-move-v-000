79f5318f2d844b4afcd384aa4d337ab827b4778a
Mai Tran <maitran215@mac.com>
2018-11-15 03:12:21 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,7 +12,7 @@ def valid_move?(board, index)
     false
   elsif index > 1 || index < 9 == true
     true
-  else index < 1 || index > 9 != true
-    true
+  else index < 1 || index > 9 == true
+    false
   end
 end
