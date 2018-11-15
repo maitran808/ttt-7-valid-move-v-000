f3b71394e24ae66329befad4677317a7de85515d
Mai Tran <maitran215@mac.com>
2018-11-15 03:18:50 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,7 +10,7 @@ end
 def valid_move?(board, index)
   if position_taken?(board, index) == true
     false
-  elsif index > 1 || index < 9 == true
+  elsif index > 0 || index < 8 == true
     true
   else index < 0 || index > 8 == true
     false
